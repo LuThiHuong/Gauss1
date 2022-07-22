62c7a6810f66130f691c8453e31f7d642f260b49
Lu Thi Huong <huong.lt195965@sis.hust.edu.vn>
2022-07-22 16:41:57 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+void khuGauss(int n) {
+    bienAThanhMatranBoSung();
+    float t;
+    for (int i = 1; i <= n; i++) {
+        if (A[i][i] == 0) {
+            printf("Err");
+            exit(0);
+        }
+        for (int k = i + 1; k <= n; k++) {
+            t = A[k][i] / A[i][i];
+            for (int j = 1; j <= n + 1; j++) {
+                A[k][j] = A[k][j] - t * A[i][j];
+            }
+        }
+    }
+}
+
+// Hàm biến đổi ma trận bổ sung dùng thuật toán Gauss-Jordan
+void khuGaussJordan(int n){
+    bienAThanhMatranBoSung();
+    float t;
+    for (int i = 1; i <= n; ++i) {
+        if (A[i][i] == 0){
+            printf("Err");
+            exit(0);
+        }
+        for (int j = 1; j <= n ; ++j) {
+            if (i != j){
+                t = A[j][i] / A[i][i];
+                for (int k = 1; k <= n+1; ++k) {
+                    A[j][k] = A[j][k] - t * A[i][k];
+                }
+            }
+        }
+    }
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
